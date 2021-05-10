0132d8116310231e8ddd4648c6c8011dbd11c50d
Mohamed.Yehia <mohamed.yehia>
2021-05-10 19:38:45 +0200

    adding user entity

--- src/main/java/com/mohyehia/ecommerce/entity/User.java
+++ src/main/java/com/mohyehia/ecommerce/entity/User.java
@@ -3,6 +3,7 @@ package com.mohyehia.ecommerce.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Data
 @Entity
@@ -14,6 +15,27 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-    @Column(name = "NAME")
-    private String name;
+    @Column(name = "USERNAME")
+    private String username;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "CREATED_AT")
+    private LocalDateTime createdAt;
+
+    @Column(name = "UPDATED_AT")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "ENABLED")
+    private boolean enabled;
+
+    @Column(name = "LOCKED")
+    private boolean locked;
 }
