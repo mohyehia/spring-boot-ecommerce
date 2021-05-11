321adcd03017a5de960f98fcbc709925d17a8034
Mohamed.Yehia <mohamed.yehia>
2021-05-11 02:40:37 +0200

    configuring the code to work with junit & mockito & moving junit dependency to org.junit.jupiter at pom.xml

--- src/main/java/com/mohyehia/ecommerce/service/impl/UserServiceImpl.java
+++ src/main/java/com/mohyehia/ecommerce/service/impl/UserServiceImpl.java
@@ -3,15 +3,13 @@ package com.mohyehia.ecommerce.service.impl;
 import com.mohyehia.ecommerce.dao.UserDAO;
 import com.mohyehia.ecommerce.entity.User;
 import com.mohyehia.ecommerce.service.framework.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserDAO userDAO;
-
-    public UserServiceImpl(UserDAO userDAO) {
-        this.userDAO = userDAO;
-    }
 
     @Override
     public User save(User user) {
