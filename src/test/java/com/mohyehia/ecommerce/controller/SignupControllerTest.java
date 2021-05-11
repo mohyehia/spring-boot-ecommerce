2da673756e7ccc33043953d26a63aaca01d79f87
Mohamed.Yehia <mohamed.yehia>
2021-05-11 18:03:46 +0200

    adding first integration test for signupController

--- src/test/java/com/mohyehia/ecommerce/controller/SignupControllerTest.java
+++ src/test/java/com/mohyehia/ecommerce/controller/SignupControllerTest.java
@@ -1,4 +1,50 @@
 package com.mohyehia.ecommerce.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.javafaker.Faker;
+import com.mohyehia.ecommerce.entity.api.request.SignupRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Locale;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class SignupControllerTest {
+    private Faker faker;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void initializeFaker() {
+        faker = new Faker(Locale.ENGLISH);
+    }
+
+    @Test
+    void when_calling_signup_endpoint_with_valid_user_then_success_and_return_201_response() throws Exception {
+        SignupRequest signupRequest = populateRandomSignupRequest();
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/v1/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(signupRequest))
+        ).andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+
+    private SignupRequest populateRandomSignupRequest() {
+        SignupRequest signupRequest = new SignupRequest();
+        signupRequest.setUsername(faker.name().username());
+        signupRequest.setEmail(faker.internet().emailAddress());
+        signupRequest.setFirstName(faker.name().firstName());
+        signupRequest.setLastName(faker.name().lastName());
+        signupRequest.setPassword(faker.internet().password(8, 20, true, true));
+        signupRequest.setConfirmPassword(signupRequest.getPassword());
+        return signupRequest;
+    }
 }
